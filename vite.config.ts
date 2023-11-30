45e1d12fc48dd4956fb8015fe1c4e562a0357253
ugk22 <tech.kjugk@gmail.com>
2023-12-01 00:00:49 +0900

    build 時に dts を生成する

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ entryRoot: resolve(__dirname, "src/lib") })],
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: resolve(__dirname, "src/lib/index.ts"),
       name: "ExampleUi",
-      fileName: "example-ui",
+      fileName: "index",
     },
   },
 });
