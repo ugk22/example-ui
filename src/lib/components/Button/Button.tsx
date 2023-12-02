cc92bfc1accdd541d25db1040b492736d82e4434
ugk22 <tech.kjugk@gmail.com>
2023-12-02 22:11:42 +0900

    Add twmerge

--- src/lib/components/Button/Button.tsx
+++ src/lib/components/Button/Button.tsx
@@ -1,10 +1,14 @@
 import { ComponentProps, FC, PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 type ButtonProps = PropsWithChildren<ComponentProps<"button">>;
 
-export const Button: FC<ButtonProps> = ({ children, ...props }) => {
+export const Button: FC<ButtonProps> = ({ children, className, ...props }) => {
   return (
-    <button className="bg-black p-4 text-white" {...props}>
+    <button
+      className={twMerge("bg-black p-4 text-white", className)}
+      {...props}
+    >
       {children}
     </button>
   );
